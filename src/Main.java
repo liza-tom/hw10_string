fcf0e87bef8a31648d871fddec68697f0eb8d3b5
tomilova.elizaveta <liza668609@mail.ru>
2026-08-18 12:48:48 +0300

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        //task 1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + ' ' + middleName + ' ' + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        //task 2
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —  " + fullName);
+
+        //task 3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
